79ba62ef5a3651d79a273d3eee4735d573f75303
forghett <foresti.adriano@gmail.com>
2023-09-13 21:20:56 +0200

    asdas

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
     const targetDate = new Date("2023-10-13").getTime();
-    const countdownContainer = document.getElementById("countdown");
-    const countdownDays = document.getElementById("countdown-days");
-    const countdownHours = document.getElementById("countdown-hours");
-    const countdownMinutes = document.getElementById("countdown-minutes");
-    const countdownSeconds = document.getElementById("countdown-seconds");
+    const countdownElements = {
+        days: document.getElementById("countdown-days"),
+        hours: document.getElementById("countdown-hours"),
+        minutes: document.getElementById("countdown-minutes"),
+        seconds: document.getElementById("countdown-seconds")
+    };
 
     function updateCountdown() {
         const currentDate = new Date().getTime();
         const timeRemaining = targetDate - currentDate;
 
         if (timeRemaining <= 0) {
-            countdownContainer.innerHTML = "Event has arrived!";
+            Object.values(countdownElements).forEach(element => {
+                element.innerHTML = "Event has arrived!";
+            });
         } else {
             const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
             const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
             const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-            countdownDays.innerHTML = days;
-            countdownHours.innerHTML = hours;
-            countdownMinutes.innerHTML = minutes;
-            countdownSeconds.innerHTML = seconds;
+            countdownElements.days.innerHTML = days;
+            countdownElements.hours.innerHTML = hours;
+            countdownElements.minutes.innerHTML = minutes;
+            countdownElements.seconds.innerHTML = seconds;
         }
     }
 
