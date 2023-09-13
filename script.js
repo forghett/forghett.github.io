7336542c26baa951dfccb011e52e9145dedc5453
forghett <foresti.adriano@gmail.com>
2023-09-13 18:52:33 +0200

    bard1

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-// Calculate the time remaining until October 13 of the current year
-const now = new Date();
-const targetDate = new Date(now.getFullYear(), 9, 13); // Month is zero-based (9 for October)
-const timeRemaining = targetDate - now;
-
-// Update the countdown timer every second
 function updateCountdown() {
-    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-
-    document.getElementById('countdown-timer').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-
-    // Animate the plane's position here based on the timeRemaining value
-    // You may need to use CSS transitions or JavaScript animation libraries
-}
-
-// Update the countdown initially
-updateCountdown();
-
-// Update the countdown every second
-setInterval(updateCountdown, 1000);
+    const now = new Date();
+    const date = new Date(2023, 9, 13, 0, 0, 0);
+    const difference = date - now;
+  
+    const daysLeft = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hoursLeft = Math.floor((difference - (daysLeft * 24 * 60 * 60)) / (1000 * 60 * 60));
+    const minutesLeft = Math.floor((difference - (daysLeft * 24 * 60 * 60) - (hoursLeft * 60 * 60)) / (1000 * 60));
+    const secondsLeft = Math.floor((difference - (daysLeft * 24 * 60 * 60) - (hoursLeft * 60 * 60) - (minutesLeft * 60)) / 1000);
+  
+    document.getElementById("daysLeft").innerHTML = daysLeft;
+    document.getElementById("hoursLeft").innerHTML = hoursLeft;
+    document.getElementById("minutesLeft").innerHTML = minutesLeft;
+    document.getElementById("secondsLeft").innerHTML = secondsLeft;
+  }
+  
+  updateCountdown();
+  
+  setInterval(updateCountdown, 1000);
+  
