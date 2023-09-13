fd5aa9c1cd4d99b85b6fc6776b130f504071aabf
forghett <foresti.adriano@gmail.com>
2023-09-13 22:27:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const targetDate = new Date("2023-10-13").getTime();
     const countdownElements = {
         days: document.getElementById("countdown-days"),
@@ -7,13 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
         seconds: document.getElementById("countdown-seconds")
     };
 
+    const erciImage = document.getElementById("erci");
+    const colosseoImage = document.getElementById("colosseo");
+
     function updateCountdown() {
-        const currentDate = new Date().getTime();
+        const currentDate = new Date().getTime(); // Use the current date
+
         const timeRemaining = targetDate - currentDate;
 
         if (timeRemaining <= 0) {
             Object.values(countdownElements).forEach(element => {
-                element.innerHTML = "Event has arrived!";
+                element.innerHTML = "Ciccio has arrived!";
             });
         } else {
             const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
@@ -25,6 +29,19 @@ document.addEventListener("DOMContentLoaded", function() {
             countdownElements.hours.innerHTML = hours;
             countdownElements.minutes.innerHTML = minutes;
             countdownElements.seconds.innerHTML = seconds;
+
+            // Calculate the progress as a percentage
+            const totalDuration = targetDate - new Date("2023-09-13").getTime();
+            const progress = 100 - (timeRemaining / totalDuration) * 100;
+
+            // Adjust the position of the "Erci" image based on the progress
+            erciImage.style.left = progress + "%";
+
+            // Calculate vertical movement based on the progress (adjust as needed)
+            const verticalMovement = (100 - progress) * 2; // You can adjust the multiplier for the desired vertical movement
+            
+            // Adjust the position of the "Erci" image vertically
+            erciImage.style.top = verticalMovement + "px";
         }
     }
 
