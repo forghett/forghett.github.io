8f35e0061114e4bb571425f2a5b8ae0f8ef11cb9
forghett <foresti.adriano@gmail.com>
2023-09-13 20:59:30 +0200

    asd

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-let days = 20;
-let hours = 0;
-let minutes = 0;
-let seconds = 0;
+document.addEventListener("DOMContentLoaded", function() {
+    const targetDate = new Date("2023-10-13").getTime();
+    const countdownContainer = document.getElementById("countdown");
+    const countdownDays = document.getElementById("countdown-days");
+    const countdownHours = document.getElementById("countdown-hours");
+    const countdownMinutes = document.getElementById("countdown-minutes");
+    const countdownSeconds = document.getElementById("countdown-seconds");
 
-function updateCountdown() {
-  seconds--;
-  if (seconds < 0) {
-    seconds = 59;
-    minutes--;
-    if (minutes < 0) {
-      minutes = 59;
-      hours--;
-      if (hours < 0) {
-        days--;
-        hours = 23;
-      }
+    function updateCountdown() {
+        const currentDate = new Date().getTime();
+        const timeRemaining = targetDate - currentDate;
+
+        if (timeRemaining <= 0) {
+            countdownContainer.innerHTML = "Event has arrived!";
+        } else {
+            const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+            countdownDays.innerHTML = days;
+            countdownHours.innerHTML = hours;
+            countdownMinutes.innerHTML = minutes;
+            countdownSeconds.innerHTML = seconds;
+        }
     }
-  }
 
-  document.getElementById("days-number").innerHTML = days;
-  document.getElementById("hours-number").innerHTML = hours;
-  document.getElementById("minutes-number").innerHTML = minutes;
-  document.getElementById("seconds-number").innerHTML = seconds
+    // Initial countdown update
+    updateCountdown();
+
+    // Update the countdown every second
+    setInterval(updateCountdown, 1000);
+});
